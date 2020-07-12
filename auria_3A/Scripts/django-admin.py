548dd24c192e5873ab44163a5a6909a130527d73
aauria <afauria@est.itssg.edu.ec>
2020-07-12 18:39:49 -0500

    Initial commit

---
+++ auria_3A/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alexander Auria\PycharmProjects\Nuevo_proyecto\auria_3A\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
